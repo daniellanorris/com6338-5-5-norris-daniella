8e19fea59564c712a64926ca40edf9ea6bce0dd0
daniellanorris <daniellanorris@ufl.edu>
2022-10-09 14:14:20 -0400

    commit

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const form = document.getElementById('add-todo')
-const input = document.querySelector('input')
-const ul = document.getElementById('todo-list');
+const form = document.getElementById("add-todo")
+const input = document.querySelector("input")
+const ul = document.getElementById("todo-list");
+const li = document.createElement("li");
+const newButton = document.createElement("button");
 
-function createButtons() {
-    const item = input.value;
-    const li = document.createElement('li');
-    const button = document.createElement('button');
-    button.textContent = item;
-    return li;
+/* add submit for form*/
+form.addEventListener('submit', addLi);
+/*main function */
+function addLi(e) {
+    e.preventDefault();
+    if(input.value) {
+    /*define new variables, create elements*/
+        const item = input.value;
+        ul.appendChild(li);
+        li.appendChild(newButton);
+        newButton.textContent = item;
+        input.value = ''
+    } 
     
+};
+
+/* create text-through for li element */
+li.onclick = function addStrike() {
+    li.style.textDecoration = "line-through"
 }
 
-form.addEventListener('click', 'submit', (event) => {
-    event.preventDefault();
-    const li = createButtons();
-    ul.appendChild(li);
-    li.appendChild(button);
-
-
-
-
-})
+/*delete li element if clicked with underline */
+li.ondblclick = function deleteItem() {
+    ul.removeChild(ul.firstElementChild)
+}
