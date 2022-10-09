3a93f0e6c07db8955694dbfe4187e740a456f0f8
daniellanorris <daniellanorris@ufl.edu>
2022-10-09 17:51:27 -0400

    commit

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const ul = document.getElementById("todo-list");
 const newButton = document.createElement("button");
 const li = document.createElement("li");
 var letters = /^[A-Za-z]+$/;
-var arr = [];
 
 /* add submit for form*/
 form.addEventListener('submit', addLi);
 /*main function */
-arr.forEach(function addLi(e) {
+function addLi(e) {
     e.preventDefault();
     if(input.value.match(letters)) {
     /*define new variables, create elements*/
         ul.appendChild(li);
         li.appendChild(newButton);
         const item = input.value;
+        newButton.textContent = item;
         input.value = ''
-        newButton.textContent = arr[item];
+      /* create line-through for li element */
+        li.onclick = function addStrike() {
+            li.style.textDecoration = "line-through"
+            }
+
+        /*delete li element if clicked with underline */
+        li.ondblclick = function deleteItem() {
+            ul.removeChild(ul.firstElementChild)
         }
-});
+    } 
 
-/* create line-through for li element */
-li.onclick = function addStrike() {
-    li.style.textDecoration = "line-through"
-    }
-
-
-/*delete li element if clicked with underline */
-li.ondblclick = function deleteItem() {
-    ul.removeChild(ul.firstElementChild)
 }
+
+  
