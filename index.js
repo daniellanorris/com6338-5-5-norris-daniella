7c32d82f087713722dcbf97e53843541ab119311
daniellanorris <daniellanorris@ufl.edu>
2022-10-09 10:27:33 -0400

    commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var form = document.querySelector('todo')
-var input = document.querySelector('input')
-form.addEventListener('submit')
-form.onsubmit = function(e) {
-    e.preventDefault();
+const form = document.getElementById('add-todo')
+const input = document.querySelector('input')
+const ul = document.getElementById('todo-list');
 
-    const itemInput = document.getElementById('add-todo');
-    console.log(itemInput.value);
-
-    const listTodo = document.getElementById('ul#todo-list');
-    console.log(listTodo.value);
-    
+function createButtons() {
+    const item = input.value;
+    const text = document.createTextNode(item);
     const li = document.createElement('li');
-    const buttonList = document.createElement('button')
-    buttonList.textContent = itemInput.value
-
-    var ul = document.querySelector('ul')
-    ul.appendChild(li)
-    li.appendChild(button)
+    const button = document.createElement('button');
+    return li;
+    
 }
 
+form.addEventListener('click', 'submit', (event) => {
+    event.preventDefault();
+    const li = createButtons();
+    ul.appendChild(li);
+    li.appendChild(button);
 
 
+
+
+})
